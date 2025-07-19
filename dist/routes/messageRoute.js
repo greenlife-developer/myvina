0661bca789b159772cf2a40bd20036f5232a070c
Opeyemi <yemijoshua81@gmail.com>
2025-07-19 06:04:22 +0100

    updating build

--- dist/routes/messageRoute.js
+++ dist/routes/messageRoute.js
@@ -1,7 +1,6 @@
 var messageExpress = require("express");
 var messageRouter = messageExpress.Router();
 var _a = require("../controllers/messageController"), getMessages = _a.getMessages, sendMessage = _a.sendMessage;
-var protectMessageRoute = require("../middleWare/authMiddleware").default;
 // console.log("User Router Loaded", protectRoute);
 messageRouter.get("/", getMessages);
 messageRouter.post("/", sendMessage);
